11825847406ea24682d9e2fd22a0349516cf81cb
thiago.melo.stuckert <thiago.melo.stuckert@gmail.com>
2021-09-07 21:31:13 -0300

    primeira versao do ping com esteganografia

--- ICMP-Ping_Dialog.py
+++ ICMP-Ping_Dialog.py
@@ -12,12 +12,16 @@ import binascii
 ICMP_ECHO_REQUEST = 8
 
 def checksum(str):
-
+#  print("debug")
+#  print(str)
   csum = 0
   countTo = (len(str) / 2) * 2
+#  print(countTo)
 
   count = 0
   while count < countTo:
+#    print(str[count])
+#    print(str[count+1])
     thisVal = str[count+1] * 256 + str[count]
     csum = csum + thisVal
     csum = csum & 0xffffffff 
@@ -59,27 +63,41 @@ def receiveOnePing(mySocket, ID, timeout, destAddr):
     print("ICMP Header: ",type, code, checksum, pID, sq)
     if pID == ID:
       bytesinDbl = struct.calcsize("d")
-      timeSent = struct.unpack("d", recPacket[28:28 + bytesinDbl])[0]
+      bytesinUnsignedInt = struct.calcsize("I")
+
+      start_pos_time = 28
+      end_pos_time = start_pos_time + bytesinDbl
+      timeSent = struct.unpack("d", recPacket[start_pos_time:end_pos_time])[0]
       rtt=timeReceived - timeSent
- 
-      print("Round-Trip Time: ")
-      return rtt
- 
+
+      #Recupera a mensagem escondida no ping
+      start_pos_msg_size = end_pos_time
+      end_pos_msg_size = start_pos_msg_size + bytesinUnsignedInt
+      (i,), msg = struct.unpack("I", recPacket[start_pos_msg_size: end_pos_msg_size]), recPacket[end_pos_msg_size: ]
+
+      return rtt, msg
+
+
     # Fill in end
 
     timeLeft = timeLeft - howLongInSelect
     if timeLeft <= 0:
       return "Request timed out."
 
-
-def sendOnePing(mySocket, destAddr, ID):
+def sendOnePing(mySocket, msg, destAddr, ID):
   # Header is type (8), code (8), checksum (16), id (16), sequence (16)
   myChecksum = 0
   # Make a dummy header with a 0 checksum.
   # struct -- Interpret strings as packed binary data
   header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0, myChecksum, ID, 1)
 
-  data = struct.pack("d", time.time())
+  timeSent = time.time()
+  data = struct.pack("d", timeSent)
+
+  #Insere a mensagem escondida no ping
+  msg_str = str(msg)
+  msg_bytes = bytes(msg_str, 'utf-8')
+  data += struct.pack("I%ds" % (len(msg_bytes),), len(msg_bytes), msg_bytes)
 
   # Calculate the checksum on the data and the dummy header.
   myChecksum = checksum(header + data)
@@ -100,7 +118,7 @@ def sendOnePing(mySocket, destAddr, ID):
   # which can be referenced by their position number within the object.
 
 
-def doOnePing(destAddr, timeout):
+def doOnePing(destAddr, msg, timeout):
 
   icmp = socket.getprotobyname("icmp")
   #SOCK_RAW is a powerful socket type. For more details:   http://sock-raw.org/papers/sock_raw
@@ -111,14 +129,14 @@ def doOnePing(destAddr, timeout):
 
   # Fill in end
   myID = os.getpid() & 0xFFFF  #Return the current process i
-  sendOnePing(mySocket, destAddr, myID)
+  sendOnePing(mySocket, msg, destAddr, myID)
   delay = receiveOnePing(mySocket, myID, timeout, destAddr)
   mySocket.close()
 
   return delay
 
 
-def ping(host, timeout=1):
+def ping(host, msg, timeout=1):
   #timeout=1 means: If one second goes by without a reply from the server,
   #the client assumes that either the client's ping or the server's pong is lost
 
@@ -129,13 +147,14 @@ def ping(host, timeout=1):
   # Send ping requests to a server separated by approximately one second.
   # I will be sending a single ping message to each server.
   print("The header fields for ICMP are: Type, Code, Checksum, ID, Sequence Number")
-  delay = doOnePing(dest, timeout) 
+  delay,msg = doOnePing(dest, msg, timeout)
   print(delay)
+  print("Mensagem escondida no ping: " + str(msg))
   time.sleep(1)# one second
 
   return delay
 
 if __name__ == '__main__': 
-	print(sys.argv[1])
-	ping(sys.argv[1])
+	print(sys.argv[1], sys.argv[2])
+	ping(sys.argv[1], sys.argv[2])
 
